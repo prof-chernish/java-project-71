990803b748575c76c3a57b8e1af1117f4b8b0d52
Ольга Чернышева <prof-chernish@rambler.ru>
2022-12-04 15:15:53 +0300

    Step 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,6 +8,14 @@ import picocli.CommandLine.Parameters;
 @Command(name = "gendiff", version = "gendiff 1.0", description = "Compares two configuration files and shows a difference.", mixinStandardHelpOptions = true)
 
 public class App {
+    @Option(names = { "-f", "--format" }, description = "output format [default: stylish]", defaultValue = "stylish")
+    String format = "stylish";
+
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private String filePath1;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private String filePath2;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
